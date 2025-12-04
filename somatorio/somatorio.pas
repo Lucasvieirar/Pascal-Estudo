aa4756ff07c97232e549623cae23259ee6ade13d
Lucasvieirar <lucasvieiraribeiro2055@gmail.com>
2025-12-04 14:42:15 +0000

    soam de 2 numeros

---
+++ somatorio/somatorio.pas
@@ -0,0 +1,9 @@
+program soma2numeros;
+
+var a, b: integer;
+
+begin
+    read (a);
+    read(b);
+    write (a+b);
+end.
---
+++ somatorio/somatorio_2.pas
@@ -0,0 +1,11 @@
+program soma2numeros_v2;
+
+var a, b: integer;
+
+begin
+   write ('Digite um numero inteiro: ');
+   read (a);
+   write ('Digite outro inteiro: ');
+   read (b);
+   writeln (a, '+', b, '=', a+b);
+end.
